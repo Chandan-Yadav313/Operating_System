2f1e69772746affce6701217902e40a0ce1739d2
Chandan-Yadav313 <chandanydv313@gmail.com>
2025-12-15 13:39:09 +0530

    created firs zero thread

--- mainThread.java
+++ mainThread.java
@@ -1,10 +1,10 @@
 
-// class threadMain extends Thread{
+class threadMain extends Thread{
 
-//     public void run(){
-//         System.out.println("Current thread is: " + Thread.currentThread().getName());
-//     }
-// }
+    public void run(){
+        System.out.println("Current thread is: " + Thread.currentThread().getName());
+    }
+}
 
 
 
@@ -12,10 +12,10 @@
 
 public class mainThread {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Current Thread: " + Thread.currentThread().getName());
-        // threadMain task0 = new threadMain();
-        // task0.start();
+        // System.out.println("Hello World!");
+        // System.out.println("Current Thread: " + Thread.currentThread().getName());
+        threadMain task0 = new threadMain();
+        task0.start();
         
     }
     
